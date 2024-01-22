89062c03e61527cb13c981493c71a6154d258acb
KaueHamasaki <123817053+kauehamasaki@users.noreply.github.com>
2024-01-21 23:51:43 -0300

    Update funcoes.js

--- javascript/funcoes.js
+++ javascript/funcoes.js
@@ -5,3 +5,13 @@ function mudafoto(foto)
 		}
 
 		
+
+function Enviar() {
+
+    var nome = document.getElementById("nomeid");
+
+    if (nome.value != "") {
+        alert('Obrigado sr(a) ' + nome.value + ' os seus dados foram encaminhados com sucesso');
+    }
+
+}
